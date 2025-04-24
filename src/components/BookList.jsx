f0d62ef28a563ca46a7e2c103db8f36b91c7f9e5
Patopam <145809595+Patopam@users.noreply.github.com>
2025-04-24 18:59:35 -0500

    .

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,11 +3,11 @@ import { useSelector } from 'react-redux';
 import BookItem from './BookItem';
 
 const BookList = () => {
-	const books = useSelector((state) => state.books.books);
-	const favorites = useSelector((state) => state.books.favorites);
+	const books = useSelector((state) => state.books.books) || [];
+	const favorites = useSelector((state) => state.books.favorites) || [];
 	const [filter, setFilter] = useState('Todos');
 
-	const categories = ['Todos', ...new Set(books.map((book) => book.category))];
+	const categories = ['Todos', ...new Set((books || []).map((book) => book.category))];
 	const filteredBooks = filter === 'Todos' ? books : books.filter((book) => book.category === filter);
 
 	return (
@@ -20,9 +20,8 @@ const BookList = () => {
 				))}
 			</select>
 
-			{filteredBooks.map((book) => (
-				<BookItem key={book.id} book={book} isFavorite={favorites.includes(book.id)} />
-			))}
+			{filteredBooks &&
+				filteredBooks.map((book) => <BookItem key={book.id} book={book} isFavorite={favorites.includes(book.id)} />)}
 		</div>
 	);
 };
--- src/components/FavoriteList.jsx
+++ src/components/FavoriteList.jsx
@@ -3,14 +3,16 @@ import { useSelector } from 'react-redux';
 import BookItem from './BookItem';
 
 const FavoriteList = () => {
-	const { books, favorites } = useSelector((state) => state.books);
-	const favoriteBooks = books.filter((book) => favorites.includes(book.id));
+	const { books = [], favorites = [] } = useSelector((state) => state.books || {});
+	const favoriteBooks = (books && books.filter((book) => favorites.includes(book.id))) || [];
 
 	return (
 		<div>
-			{favoriteBooks.map((book) => (
-				<BookItem key={book.id} book={book} isFavorite={true} />
-			))}
+			{favoriteBooks.length > 0 ? (
+				favoriteBooks.map((book) => <BookItem key={book.id} book={book} isFavorite={true} />)
+			) : (
+				<p>No hay libros favoritos</p>
+			)}
 		</div>
 	);
 };
